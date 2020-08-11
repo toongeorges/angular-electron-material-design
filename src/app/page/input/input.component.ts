ae55d15d0f80f50bd39ac2ea63d256b4b56fdae9
Toon Macharis <Toon.Macharis@gmail.com>
2020-08-11 16:07:59 +0200

    add an input for selecting a folder

--- src/app/page/input/input.component.ts
+++ src/app/page/input/input.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators, FormGroupDirective, NgForm } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
+import { ElectronService } from 'app/core/services/electron/electron.service';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -17,6 +18,7 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 })
 export class InputComponent implements OnInit {
   value = 'Clear me';
+  folder: string = "";
 
   emailFormControl = new FormControl('', [
     Validators.required,
@@ -25,9 +27,25 @@ export class InputComponent implements OnInit {
 
   matcher = new MyErrorStateMatcher();
 
-  constructor() { }
+  constructor(
+    private electronService: ElectronService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  openFolderDialog() {
+    //see https://www.electronjs.org/docs/api/dialog
+    this.electronService.remote.dialog.showOpenDialog(
+      { properties: ['openDirectory'] }
+    ).then(result => {
+      console.log("The folder has been selected: " + !result.canceled)
+      console.log("The selected folder: " + result.filePaths[0])
+      if (!result.canceled) {
+        this.folder = result.filePaths[0];
+      }
+    }).catch(err => {
+      console.log(err);
+    });
+  }
 }
